96fba24c3d457497dc77eb23eee0619579187cdf
Sergey <seregad07@mail.ru>
2022-09-20 00:44:27 +0700

    homework is done

---
+++ task_41/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArray(int size)
+            {
+            int[] mas = new int[size];
+
+            for (int i=0;i<size;i++) 
+                {
+                    Console.WriteLine($"Enter INTEGER {i}-element");
+                    mas[i] = int.Parse(Console.ReadLine());
+                    // mas[i]= new Random().Next(-100,101);
+                }
+            return mas;
+            }
+void PrintArray(int[] m)
+            {
+            for (int i=0;i<m.Length;i++) 
+                {
+                    if (i!=m.Length-1) Console.Write($"{m[i]}, ");
+                    else Console.WriteLine($"{m[i]}");
+                }
+            }
+
+int CheckPositive (int [] m)
+{
+    int count = 0;
+    for (int i=0;i<m.Length;i++)
+    {
+        if ( m[i] > 0) count++;
+    }
+    return count;
+}
+
+Console.WriteLine("Enter dimension of your massive"); 
+try 
+{
+    int m = int.Parse(Console.ReadLine());
+    int[] array = FillArray(m);
+    Console.Write("Your massive is: "); 
+    PrintArray(array);
+    Console.WriteLine($"There was/were {CheckPositive(array)} elements in your massive");
+}
+catch 
+{
+    Console.WriteLine("You've entered something wrong"); 
+}
---
+++ task_43/Program.cs
@@ -0,0 +1,101 @@
+﻿// // //  Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// // double CheckDot (int b1, int b2, int k1, int k2)
+// // {
+// //         double sum1 =0;
+// //         double sum2 =-1;
+// //         double rez;
+// //        for (double x = -100; x < 100; x = x+0.01) 
+// //             {
+// //             sum1 = k1*x+b1;
+// //             sum2 = k2*x+b2;
+// //             if (sum1 == sum2) rez = x;
+// //             else
+// //             {
+// //                 sum1 =0;
+// //                 sum2=0;
+// //                 rez=0;
+// //             }
+// //             }
+// //         return sum1;   
+// // }
+
+// // try
+// // {
+// //     Console.WriteLine("Enter b1 ");
+// //     int b1 = int.Parse(Console.ReadLine());
+// //     Console.WriteLine("Enter k1 ");
+// //     int k1 = int.Parse(Console.ReadLine());
+// //     Console.WriteLine("Enter b2 ");
+// //     int b2 = int.Parse(Console.ReadLine());
+// //     Console.WriteLine("Enter k2 ");
+// //     int k2 = int.Parse(Console.ReadLine());
+// //     double res1 = 0;
+// //     double res2 = 0;
+// //     res1 = CheckDot(b1, b2, k1, k2);
+// //     res2 = k1*res1+b1;
+// //     Console.WriteLine(res1+" "+res2);
+// // }
+// // catch 
+// // {
+// //    Console.WriteLine("You've entered something wrong"); 
+// // }
+
+// Console.WriteLine("Enter b1 ");
+//     int b1 = int.Parse(Console.ReadLine());
+//     Console.WriteLine("Enter k1 ");
+//     int k1 = int.Parse(Console.ReadLine());
+//     Console.WriteLine("Enter b2 ");
+//     int b2 = int.Parse(Console.ReadLine());
+//     Console.WriteLine("Enter k2 ");
+//     int k2 = int.Parse(Console.ReadLine());
+
+// double CheckDot (int b1, int b2, int k1, int k2)
+// {
+//         double sum1 =0;
+//         double sum2 =-1;
+//         double rez;
+//        for (double x = -10; x < 10; x = x+0.5) 
+//             {
+//             sum1 = k1*x+b1;
+//             sum2 = k2*x+b2;
+//             if (sum1 == sum2) rez = x;
+//             else
+//             {
+//                 sum1=0;
+//                 sum2=0;
+//             }
+//             }
+//         return sum1;   
+// }
+
+//     double res1 = CheckDot (b1, b2, k1, k2);
+//     double res2 = k1*res1+b1;
+// Console.WriteLine(res1+" "+res2);
+
+Console.WriteLine("Введите уравнения двух прямых вида у = Ki*x + Bi");
+
+Double[] array = new Double[4];
+
+Console.WriteLine("Введите коэффициент K1");
+array[0] = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффициент B1");
+array[1]  = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффициент K2");
+array[2]  = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите коэффициент B2");
+array[3]  = double.Parse(Console.ReadLine());
+try
+{    
+double rezX = (array[3]-array[1])/(array[0]-array[2]);
+double rezY = array[0]*rezX + array[1];
+Console.WriteLine($"Прямые y = {array[0]}*x + {array[1]}  и  y = {array[2]}*x + {array[3]} пересекаются в точке с координатами:");
+if (array[0] == array[2] && array[1] == array[3]) Console.WriteLine("Коэффициенты K1 и K2, а так же B1 B2 должны отличаться");
+else Console.WriteLine("x= "+ rezX +" y= "+ rezY);
+}
+catch
+{
+    Console.WriteLine("Коэффициенты должны быть числами");
+}
